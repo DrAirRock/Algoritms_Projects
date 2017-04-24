1e03874c76c449bd09e4efef594c32162c0c709a
Eric Songefest <ericsognefest@gmail.com>
2017-04-24 19:44:12 -0400

    stuff

--- Project_2/src/Tree.h
+++ Project_2/src/Tree.h
@@ -1,3 +1,5 @@
+#ifndef TREE_H
+#define TREE_H
 //implametation of tree class
 
 template<class K, class V>
@@ -7,8 +9,11 @@ class Node{
     K key_;
     V value_;
     Node *right_, *left_, *parent_;
-
-    Node() { right_ = NULL; left_ = NULL; parent_ = NULL;} 
+    Node() { right_ = NULL; left_ = NULL; parent_ = NULL;}
 
 
 };
+
+
+
+#endif
